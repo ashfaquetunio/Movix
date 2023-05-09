dd267c81337905c2d87db4e7b3b529ebbe50470b
Ashfaque Ahmed <ahmedashfaque522@gmail.com>
2023-05-09 12:23:10 +0500

    TMDB API Setup
    Setting up an api connection for movies data to be listed on the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
+  useEffect(() => {
+    testApi();
+  }, []);
+  const testApi = () => {
+    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+  };
   return (
     <>
       <div className="app"></div>
